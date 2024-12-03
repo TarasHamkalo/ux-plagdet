1644bb3c7b44fa8bd0af64623589f163671c1901
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-03 10:43:28 +0100

    feat(header): add header emitting menu toggle event

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbar,
+    MatIcon,
+    MatIconButton,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  @Output() public menuToggle = new EventEmitter<void>();
+
+  onMenuToggle(): void {
+    this.menuToggle.emit();
+  }
 
 }
