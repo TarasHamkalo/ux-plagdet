aeddd69bbf53dbe730887d013081ad5d57aa7969
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-03 10:44:01 +0100

    feat(home): add home which manages sidenav and content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
-import {MatNavList, MatListItem} from "@angular/material/list";
+import {MatListItem, MatNavList} from "@angular/material/list";
 import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
 
 // Combines side navigation-gen and content
 @Component({
@@ -10,10 +9,17 @@ import {MatIconButton} from "@angular/material/button";
   imports: [
     MatNavList, MatListItem,
     MatSidenav, MatSidenavContainer, MatSidenavContent,
-    MatIcon, MatIconButton
+    MatIcon
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  public isSidenavOpen = signal(false);
+
+  toggleSideNav() {
+    this.isSidenavOpen.update(b => !b)
+  }
+
 }
