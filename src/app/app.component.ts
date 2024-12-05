56bc800e2d76e26888c67c3f870d29ff1a745b41
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-05 21:55:25 +0100

    feat(icon-registry): add custom icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component} from "@angular/core";
 import {HeaderComponent} from "./components/header/header.component";
 import {HomeComponent} from "./home/home.component";
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
   ],
 
   template: `
@@ -25,13 +27,34 @@ import {HomeComponent} from "./home/home.component";
       display: flex;
       flex-direction: column;
     }
-    
+
     .content {
       flex-grow: 1;
-      padding-top: 64px; 
+      padding-top: 64px;
     }
   `
   // TODO: change to rem ?
 })
 export class AppComponent {
+
+  constructor(private iconRegistry: MatIconRegistry,
+              private domSanitizer: DomSanitizer
+  ) {
+
+    const icons: string[] = [
+      "file-document-multiple-outline",
+      "rename-box-outline"
+    ]
+
+    this.registerIcons(icons);
+  }
+
+  private registerIcons(icons: string[]) {
+    for (const icon of icons) {
+      this.iconRegistry.addSvgIcon(
+        icon,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`./assets/${icon}.svg`)
+      );
+    }
+  }
 }
