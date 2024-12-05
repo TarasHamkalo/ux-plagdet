95c53036f8f6c2252c9484b6dbfbe82333ee00c1
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-05 20:32:43 +0100

    feat(file-upload-dnd): add file emit

--- src/app/components/file-upload-dnd/file-upload-dnd.component.ts
+++ src/app/components/file-upload-dnd/file-upload-dnd.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {DndDirective} from "../../directives/dnd.directive";
+import {AnalysisContextService} from "../../services/analysis-context.service";
 
 @Component({
   selector: 'app-file-upload-dnd',
   imports: [
     MatButton,
-    DndDirective
+    DndDirective,
   ],
   templateUrl: './file-upload-dnd.component.html',
   styleUrl: './file-upload-dnd.component.css'
@@ -21,9 +22,11 @@ export class FileUploadDndComponent {
 
   @Input() public supportedExtensions: Set<string> = new Set<string>(['tar', 'zip', 'targz']);
 
+  @Output() public fileUploaded: EventEmitter<File> = new EventEmitter<File>();
+
   public onFileSelect(event: any): void {
     if (event.target) {
-      this.processFiles(event.target.files)
+      this.processFiles(event.target.files);
     }
   }
 
@@ -32,22 +35,24 @@ export class FileUploadDndComponent {
       this.processFiles(event.dataTransfer.files);
     }
   }
-  
+
+  // TODO: probably should be moved to a service...
   private processFiles(files: FileList): void {
     if (files.length > 1) {
-      console.log('Error number of files should be equal 1')
+      console.log('Error number of files should be equal 1');
       return;
     }
 
     const file = files[0];
     if (this.hasValidExtension(file)) {
-
+      this.fileUploaded.emit(file)
     }
   }
 
   private hasValidExtension(file: File): boolean {
-    const ext = file.type.split('/')[1]
-    return this.supportedExtensions.has(ext)
+    const ext = file.type.split('/')[1];
+    return this.supportedExtensions.has(ext);
   }
+
 
 }
