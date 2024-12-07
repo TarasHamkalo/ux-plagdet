51b1952347d321914df77f39b9580df0bb31c4be
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-07 19:20:11 +0100

    feat(configuration-page): add analysis name saving to context

--- src/app/pages/configuration-page/configuration-page.component.ts
+++ src/app/pages/configuration-page/configuration-page.component.ts
@@ -4,13 +4,14 @@ import {
   ConfigurationOptionsComponent
 } from "../../components/configuration-options/configuration-options.component";
 import {AnalysisContextService} from "../../services/analysis-context.service";
-import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatError, MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatIcon} from "@angular/material/icon";
 import {StopPropagationDirective} from "../../directives/stop-propagation.directive";
 import {FileWrapper} from "../../model/file-wrapper.type";
 import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatActionList, MatListItem} from "@angular/material/list";
+import {Router} from "@angular/router";
+import {FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-configuration-page',
@@ -24,32 +25,40 @@ import {MatActionList, MatListItem} from "@angular/material/list";
     MatLabel,
     MatSuffix,
     StopPropagationDirective,
-    MatActionList,
     MatButton,
-    MatListItem
+    MatError,
+    ReactiveFormsModule
   ],
   templateUrl: './configuration-page.component.html',
   styleUrl: './configuration-page.component.css'
 })
 export class ConfigurationPageComponent {
 
-  public uploadedFile = signal<Partial<FileWrapper>>({})
+  public readonly analysisNameControl = new FormControl(
+    '', [Validators.required, Validators.minLength(3)]
+  );
 
-  @Input() public placeHolderText: string = "Nastavte názov analýzy";
+  @Input() public formFieldLabel: string = 'Názov analýzy';
 
-  @Input() public formFieldLabel: string = "Názov analýzy";
+  @Input() public errorMessage: string = 'Je potrebné nastaviť názov';
 
-  public countSelected = computed(() => this.analysisContext.getConfigurationOptions()().reduce(
-    (acc, option) => acc + (option.selected ? 1 : 0), 0
-  ));
-
-  constructor(private analysisContext: AnalysisContextService) {
+  constructor(private analysisContext: AnalysisContextService,
+              private router: Router) {
   }
 
   ngOnInit() {
-    this.uploadedFile = this.analysisContext.getUploadedFile()
-    console.log(this.uploadedFile())
-    this.placeHolderText = this.uploadedFile().name ?? "Nastavte názov analýzy";
+    this.analysisNameControl.setValue(this.analysisContext.getAnalysisName()())
   }
 
+  onCancel() {
+    this.router.navigate(['/upload']);
+  }
+
+  onProceed() {
+    const analysisName = this.analysisNameControl.getRawValue()
+    if (analysisName) {
+      this.analysisContext.getAnalysisName().set(analysisName);
+      this.router.navigate(['/analysis']);
+    }
+  }
 }
--- src/app/services/analysis-context.service.ts
+++ src/app/services/analysis-context.service.ts
@@ -11,20 +11,27 @@ export class AnalysisContextService {
 
   private uploadedFile = signal<Partial<FileWrapper>>({});
 
+  private analysisName = signal('');
+
   private configurationOptions = signal<ConfigurationOption[]>([]);
 
   constructor(private http: HttpClient,
               private fileUtils: FileUtilsService) {
-    this.uploadedFile.set(this.fileUtils.mockWrapper());
+    this.setUploadedFile(this.fileUtils.mockWrapper());
   }
 
 
   setUploadedFile(file: Partial<FileWrapper>) {
     this.uploadedFile.set(file);
+    this.analysisName.set(file.name ?? '')
   }
 
   getUploadedFile() {
     return this.uploadedFile;
+  }
+
+  getAnalysisName() {
+    return this.analysisName;
   }
 
   loadConfigurationOptions() {
