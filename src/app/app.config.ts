a948b832d9cb74a2ac55708b17a025f1fe5e77a7
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-09 20:55:19 +0100

    fix!: slovak translation of table paginator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import {provideRouter} from "@angular/router";
 import {routes} from "./app.routes";
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {MatPaginatorIntl} from "@angular/material/paginator";
+import {SlovakPaginatorService} from "./services/slovak-paginator.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptorsFromDi()),
+    {provide: MatPaginatorIntl, useClass: SlovakPaginatorService}
   ]
 };
---
+++ src/app/services/slovak-paginator.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SlovakPaginatorService } from './slovak-paginator.service';
+
+describe('SlovakPaginatorService', () => {
+  let service: SlovakPaginatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SlovakPaginatorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
