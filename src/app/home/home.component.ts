5f2eb04dba5790f2146c3ab13675866b57be3ee5
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-09 18:31:33 +0100

    fix: body color and fixed navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,12 @@ import {Component, signal} from '@angular/core';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from "@angular/material/sidenav";
 import {NavListComponent} from "../components/nav-list/nav-list.component";
 import {RouterOutlet} from "@angular/router";
-import {SubmissionsListComponent} from "../components/submissions-list/submissions-list.component";
 
 // Combines side navigation-gen and content
 @Component({
   selector: 'app-home',
   imports: [
-    MatSidenav, MatSidenavContainer, MatSidenavContent, NavListComponent, RouterOutlet, SubmissionsListComponent
+    MatSidenav, MatSidenavContainer, MatSidenavContent, NavListComponent, RouterOutlet
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
