838b6d1cd72b1c8e35e728a2560d3654125f48ac
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-12-09 18:38:01 +0100

    feat(header): add account circle, title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,21 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {MatToolbar} from '@angular/material/toolbar';
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   imports: [
     MatToolbar,
     MatIcon,
     MatIconButton,
   ],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css"
 })
 export class HeaderComponent {
+
+  @Input() public title = "Plagdet";
 
   @Output() public menuToggle = new EventEmitter<void>();
 
